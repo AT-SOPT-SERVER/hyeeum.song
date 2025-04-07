b1d37767d6945fe021554fb21d94932c60002c17
hyeumm <20221159@sungshin.ac.kr>
2025-04-07 23:28:18 +0900

    [Add/#5] add seminar week1 code

---
+++ src/main/java/org/sopt/controller/PostController.java
@@ -0,0 +1,38 @@
+package org.sopt.controller;
+
+import org.sopt.domain.Post;
+import org.sopt.service.PostService;
+
+import java.util.List;
+
+//요청에 따라 서비스를 결정 - 요청을 받음
+public class PostController {
+    private PostService postService = new PostService();
+    private int postId;
+
+
+//    public Object selectMethod(int inputCase) {
+//        switch (inputCase) {
+//            case 1:
+//                createPost();
+//
+//        }
+//    }
+
+    public void createPost(String title) {
+        Post post = new Post(postId++, title);
+        postService.createPost(post);
+    }
+
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    public Post getPostById(int id) {
+        return postService.findPostById(id);
+    }
+
+    public boolean deletePostById(int id) {
+        return postService.deletePostById(id);
+    }
+}
---
+++ src/main/java/org/sopt/service/PostService.java
@@ -0,0 +1,27 @@
+package org.sopt.service;
+
+import org.sopt.domain.Post;
+import org.sopt.repository.PostRepository;
+
+import java.util.List;
+
+//레포지토리를 가져와서 사용하는 역할 (저장소 가져오기) - 로직
+public class PostService {
+    private PostRepository postRepository = new PostRepository();
+
+    public void createPost(Post post) {
+        postRepository.save(post);
+    }
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public Post findPostById(int id) {
+        return postRepository.findPostById(id);
+    }
+
+    public boolean deletePostById(int id) {
+        return postRepository.deletePostById(id);
+    }
+}
