b2df8b4695db9d3874f5c13bb977f503bf0eef3c
hyeumm <20221159@sungshin.ac.kr>
2025-04-10 10:40:41 +0900

    [Feature/#5] optional - allow post after 3 minutes

--- src/main/java/org/sopt/controller/PostController.java
+++ src/main/java/org/sopt/controller/PostController.java
@@ -3,7 +3,10 @@ package org.sopt.controller;
 import org.sopt.domain.Post;
 import org.sopt.service.PostService;
 import org.sopt.util.IdentifierGeneratorUtil;
+import org.sopt.util.LastTimeStampGeneratorUtil;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 //요청에 따라 서비스를 결정 - 요청을 받음
@@ -11,6 +14,17 @@ public class PostController {
     private PostService postService = new PostService();
 
     public void createPost(String title) {
+        // TODO: 3분이 지났는지 판별하는 로직을 어디에 두어야할까? 흐음
+        LocalDateTime currentTime = LocalDateTime.now();
+
+        if (LastTimeStampGeneratorUtil.getLastTimeStamp() == null)
+            LastTimeStampGeneratorUtil.setLastTimeStamp(currentTime);
+        else {
+            Duration duration = Duration.between(LastTimeStampGeneratorUtil.getLastTimeStamp(), currentTime);
+            if (duration.toMinutes() < 3)
+                throw new IllegalArgumentException("새 게시글은 3분이 지나고 난 후에 작성 가능합니다.");
+        }
+
         Post post = new Post(IdentifierGeneratorUtil.generateIdentifier(), title);
         postService.createPost(post);
     }
