655cc68ef738de8cfc56db21a49d59e6f26bb646
hyeumm <20221159@sungshin.ac.kr>
2025-04-24 12:33:50 +0900

    [Chore/#7] edit PostRepository class to interface

--- src/main/java/org/sopt/repository/PostRepository.java
+++ src/main/java/org/sopt/repository/PostRepository.java
@@ -1,47 +1,9 @@
 package org.sopt.repository;
 
 import org.sopt.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-public class PostRepository {
-    List<Post> postList = new ArrayList<>();
-
-    public void save(final Post post) {
-        postList.add(post);
-    }
-
-    public List<Post> findAll() {
-        return postList;
-    }
-
-    public Optional<Post> findPostById(final long id) {
-        for (Post post : postList) {
-            if (post.getId() == id) {
-                return Optional.of(post);
-            }
-        }
-        return Optional.empty();
-    }
-
-    public boolean deletePostById(final long id) {
-        return postList.removeIf(post -> post.getId() == id);
-    }
-
-    public List<Post> searchPostsByKeyword(final String keyword) {
-        return postList.stream()
-                .filter(post -> post.getTitle().contains(keyword))
-                .collect(Collectors.toList());
-    }
-
-    public boolean isTitleDuplicated(final String title) {
-        for (Post postList : postList) {
-            if (title.equals(postList.getTitle()))
-                return true;
-        }
-        return false;
-    }
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
