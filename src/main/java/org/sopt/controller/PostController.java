9ab09cb92b2483050f6981521c72b2d4039d090a
hyeumm <20221159@sungshin.ac.kr>
2025-04-24 13:48:38 +0900

    [Chore/#7] apply UriConstant

--- src/main/java/org/sopt/controller/PostController.java
+++ src/main/java/org/sopt/controller/PostController.java
@@ -1,5 +1,6 @@
 package org.sopt.controller;
 
+import org.sopt.constant.UriConstant;
 import org.sopt.dto.PostRequest;
 import org.sopt.service.PostService;
 import org.springframework.http.ResponseEntity;
@@ -13,32 +14,32 @@ public class PostController {
         this.postService = postService;
     }
 
-    @PostMapping("/posts")
+    @PostMapping(UriConstant.POSTS)
     public void createPost(@RequestBody final PostRequest postRequest) {
         postService.createPost(postRequest.getTitle());
     }
 
-    @GetMapping("/posts")
+    @GetMapping(UriConstant.POSTS)
     public ResponseEntity<?> getAllPosts() {
         return ResponseEntity.ok(postService.getAllPosts());
     }
 
-    @GetMapping("/posts/{id}")
+    @GetMapping(UriConstant.POST_BY_ID_URI)
     public ResponseEntity<?> getPostById(@PathVariable("id") final long id) {
         return ResponseEntity.ok(postService.findPostById(id));
     }
 
-    @DeleteMapping("/posts/{id}")
+    @DeleteMapping(UriConstant.POST_BY_ID_URI)
     public ResponseEntity<?> deletePostById(@PathVariable("id") final long id) {
         return ResponseEntity.ok(postService.deletePostById(id));
     }
 
-    @PutMapping("/posts/{id}")
+    @PutMapping(UriConstant.POST_BY_ID_URI)
     public ResponseEntity<?> updatePostTitle(@PathVariable("id") final long updateId, @RequestBody String newTitle) {
         return ResponseEntity.ok(postService.updatePostTitle(updateId, newTitle));
     }
 
-    @GetMapping("posts/search")
+    @GetMapping(UriConstant.SEARCH_POSTS_URI)
     public ResponseEntity<?> searchPostsByKeyword(@RequestParam("keyword") final String keyword) {
         return ResponseEntity.ok(postService.searchPostsByKeyword(keyword));
     }
