f5f6592140d1f7024131539e64c54ac1539b756a
hyeumm <20221159@sungshin.ac.kr>
2025-04-24 17:32:00 +0900

    [Chore/#7] apply custom exception

--- src/main/java/org/sopt/service/PostService.java
+++ src/main/java/org/sopt/service/PostService.java
@@ -1,7 +1,9 @@
 package org.sopt.service;
 
 import org.sopt.domain.Post;
-import org.sopt.exception.CustomException;
+import org.sopt.exception.TitleBlankException;
+import org.sopt.exception.TitleDuplicatedException;
+import org.sopt.exception.TitleLengthException;
 import org.sopt.repository.PostRepository;
 import org.sopt.validator.TitleValidator;
 import org.springframework.stereotype.Service;
@@ -12,7 +14,6 @@ import java.util.Optional;
 
 import static org.sopt.constant.PostConstant.POST_TIME_LIMIT;
 import static org.sopt.constant.PostConstant.TITLE_LENGTH_LIMIT;
-import static org.sopt.exception.Error.*;
 import static org.sopt.util.LastTimeStampGeneratorUtil.getLastTimeStamp;
 import static org.sopt.util.LastTimeStampGeneratorUtil.setLastTimeStamp;
 import static org.sopt.validator.TimeStampValidator.validateLastTimeStampLimit;
@@ -61,7 +62,7 @@ public class PostService {
         return true;
     }
 
-        public void validateTimeStamp() {
+    public void validateTimeStamp() {
         LocalDateTime lastTimeStamp = getLastTimeStamp();
 
         if (lastTimeStamp == null) return;
@@ -69,13 +70,13 @@ public class PostService {
     }
 
     public void validateTitle(final String title) {
-        if (TitleValidator.isTitleBlank(title)) throw new CustomException(TITLE_BLANK_ERROR);
+        if (TitleValidator.isTitleBlank(title)) throw new TitleBlankException();
         if (TitleValidator.isTitleExceedsLength(title, TITLE_LENGTH_LIMIT))
-            throw new CustomException(TITLE_LENGTH_ERROR);
+            throw new TitleLengthException();
         // TO.파트장님 !!!!!!!!!!!!!!!!!!!!!!!
         // 요기 아래처럼 해도 괜찮나요? Validator 를 사용해서 구현하고 싶었는데 아무래도 list 전체에 접근해야해서 Repository 에서 메소드를 불러왔어요
         if (postRepository.isTitleDuplicated(title))
-            throw new CustomException(TITLE_DUPLICATED_ERROR);
+            throw new TitleDuplicatedException();
     }
 
     public List<Post> searchPostsByKeyword(final String keyword) {
