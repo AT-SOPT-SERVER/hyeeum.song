b4b5caa716a25bb177cb873b6f98acf2f55fcb0c
hyeumm <20221159@sungshin.ac.kr>
2025-04-25 08:40:02 +0900

    [Chore/#7] apply args to CustomException

--- src/main/java/org/sopt/exception/CustomException.java
+++ src/main/java/org/sopt/exception/CustomException.java
@@ -2,13 +2,19 @@ package org.sopt.exception;
 
 public class CustomException extends RuntimeException {
     private final Error code;
+    private final Object[] args;
 
     public CustomException(Error error, Object... args) {
         super(error.getErrorMessage(args));
         this.code = error;
+        this.args = args;
     }
 
     public Error getCode() {
         return code;
+    }
+
+    public Object[] getArgs() {
+        return args;
     }
 }
