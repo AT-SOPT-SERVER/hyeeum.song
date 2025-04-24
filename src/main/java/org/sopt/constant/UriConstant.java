7cc60272ec1f4866c98b043bd35c6198574578e8
hyeumm <20221159@sungshin.ac.kr>
2025-04-24 13:48:20 +0900

    [Add/#7] add UriConstant

---
+++ src/main/java/org/sopt/constant/UriConstant.java
@@ -0,0 +1,10 @@
+package org.sopt.constant;
+
+public class UriConstant {
+    public static final String POSTS = "/posts";
+    public static final String PATH_ID = "/{id}";
+    public static final String SEARCH = "/search";
+
+    public static final String POST_BY_ID_URI = POSTS + PATH_ID;
+    public static final String SEARCH_POSTS_URI = POSTS + SEARCH;
+}
