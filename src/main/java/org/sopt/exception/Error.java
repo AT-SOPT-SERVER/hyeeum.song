367c523a8283c349f4c693c60f4cf5681c018716
hyeumm <20221159@sungshin.ac.kr>
2025-04-24 15:37:29 +0900

    [Chore/#7] edit error code/message

--- src/main/java/org/sopt/exception/Error.java
+++ src/main/java/org/sopt/exception/Error.java
@@ -1,15 +1,34 @@
 package org.sopt.exception;
 
 public enum Error {
-    TITLE_BLANK_ERROR("제목에 공백을 입력할 수 없습니다"),
-    TITLE_LENGTH_ERROR("제목은 %d글자 이상 입력할 수 없습니다"),
-    TITLE_DUPLICATED_ERROR("이미 존재하는 제목입니다"),
-    TIME_STAMP_LIMIT_ERROR("마지막 게시글 작성시간에서 %d분이 지나지 않았습니다");
+    // 400 Bad Request
+    TITLE_BLANK_ERROR(40001, "제목은 비어있을 수 없습니다."),
+    TITLE_LENGTH_ERROR(40002, "제목의 길이는 %d자를 넘을 수 없습니다."),
+    POST_TIME_LIMIT_ERROR(40003, "마지막 게시글 작성 시간 %d분 후에 작성이 가능합니다."),
 
+    // 404 Not Found
+    INVALID_URL_ERROR(40401, "지원하지 않는 URL입니다."),
+    POST_NOT_FOUND_ERROR(40402, "존재하지 않는 게시물입니다."),
+
+    // 405 Method Not Allowed
+    METHOD_NOT_ALLOWED_ERROR(40501, "잘못된 HTTP method 요청입니다."),
+
+    // 409 Conflict
+    TITLE_DUPLICATED_ERROR(40901, "중복된 게시글 제목입니다."),
+
+    // 500 Internal Server Error
+    INTERNAL_SERVER_ERROR(50001, "서버 내부 오류입니다.");
+
+    private final long errorCode;
     private final String errorMessage;
 
-    Error(String errorMessage) {
+    Error(long errorCode, String errorMessage) {
+        this.errorCode = errorCode;
         this.errorMessage = errorMessage;
+    }
+
+    public long getErrorCode() {
+        return errorCode;
     }
 
     public String getErrorMessage(final Object... args) {
