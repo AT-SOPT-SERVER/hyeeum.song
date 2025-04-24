29f67b35f47150531b1d66dfe5adf32a3c2f44ab
hyeumm <20221159@sungshin.ac.kr>
2025-04-24 12:55:49 +0900

    [Chore/#7] apply spring framework to PostController - getPostById

--- src/main/java/org/sopt/controller/PostController.java
+++ src/main/java/org/sopt/controller/PostController.java
@@ -3,9 +3,7 @@ package org.sopt.controller;
 import org.sopt.domain.Post;
 import org.sopt.dto.PostRequest;
 import org.sopt.service.PostService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,19 +25,20 @@ public class PostController {
         return postService.getAllPosts();
     }
 
-    public Optional<Post> getPostById(final long id) {
+    @GetMapping("/post/{id}")
+    public Optional<Post> getPostById(@PathVariable("id") final long id) {
         return postService.findPostById(id);
     }
 
-    public boolean deletePostById(final long id) {
-        return postService.deletePostById(id);
-    }
-
-    public boolean updatePostTitle(final long updateId, String newTitle) {
-        return postService.updatePostTitle(updateId, newTitle);
-    }
-
-    public List<Post> searchPostsByKeyword(String keyword) {
-        return postService.searchPostsByKeyword(keyword);
-    }
+//    public boolean deletePostById(final long id) {
+//        return postService.deletePostById(id);
+//    }
+//
+//    public boolean updatePostTitle(final long updateId, String newTitle) {
+//        return postService.updatePostTitle(updateId, newTitle);
+//    }
+//
+//    public List<Post> searchPostsByKeyword(String keyword) {
+//        return postService.searchPostsByKeyword(keyword);
+//    }
 }
--- src/main/java/org/sopt/service/PostService.java
+++ src/main/java/org/sopt/service/PostService.java
@@ -25,8 +25,8 @@ public class PostService {
     }
 
     public void createPost(final String title) {
-        validateTimeStamp();
-        validateTitle(title);
+//        validateTimeStamp();
+//        validateTitle(title);
 
         Post post = new Post(title);
         setLastTimeStamp();
@@ -39,41 +39,41 @@ public class PostService {
     }
 
     public Optional<Post> findPostById(final long id) {
-        return postRepository.findPostById(id);
+        return postRepository.findById(id);
     }
 
-    public boolean deletePostById(final long id) {
-        return postRepository.deletePostById(id);
-    }
-
-    public boolean updatePostTitle(final long updateId, String newTitle) {
-        validateTitle(newTitle);
-
-        Optional<Post> post = postRepository.findPostById(updateId);
-        if (post.isEmpty()) return false;
-
-        post.get().updateTitle(newTitle);
-        return true;
-    }
-
-    public void validateTimeStamp() {
-        LocalDateTime lastTimeStamp = getLastTimeStamp();
-
-        if (lastTimeStamp == null) return;
-        validateLastTimeStampLimit(getLastTimeStamp(), POST_TIME_LIMIT);
-    }
-
-    public void validateTitle(final String title) {
-        if (TitleValidator.isTitleBlank(title)) throw new IllegalArgumentException(TITLE_BLANK_ERROR.getErrorMessage());
-        if (TitleValidator.isTitleExceedsLength(title, TITLE_LENGTH_LIMIT))
-            throw new IllegalArgumentException(TITLE_LENGTH_ERROR.getErrorMessage());
-        // TO.파트장님 !!!!!!!!!!!!!!!!!!!!!!!
-        // 요기 아래처럼 해도 괜찮나요? Validator 를 사용해서 구현하고 싶었는데 아무래도 list 전체에 접근해야해서 Repository 에서 메소드를 불러왔어요
-        if (postRepository.isTitleDuplicated(title))
-            throw new IllegalArgumentException(TITLE_DUPLICATED_ERROR.getErrorMessage());
-    }
-
-    public List<Post> searchPostsByKeyword(final String keyword) {
-        return postRepository.searchPostsByKeyword(keyword);
-    }
+//    public boolean deletePostById(final long id) {
+//        return postRepository.deletePostById(id);
+//    }
+//
+//    public boolean updatePostTitle(final long updateId, String newTitle) {
+//        validateTitle(newTitle);
+//
+//        Optional<Post> post = postRepository.findPostById(updateId);
+//        if (post.isEmpty()) return false;
+//
+//        post.get().updateTitle(newTitle);
+//        return true;
+//    }
+//
+//    public void validateTimeStamp() {
+//        LocalDateTime lastTimeStamp = getLastTimeStamp();
+//
+//        if (lastTimeStamp == null) return;
+//        validateLastTimeStampLimit(getLastTimeStamp(), POST_TIME_LIMIT);
+//    }
+//
+//    public void validateTitle(final String title) {
+//        if (TitleValidator.isTitleBlank(title)) throw new IllegalArgumentException(TITLE_BLANK_ERROR.getErrorMessage());
+//        if (TitleValidator.isTitleExceedsLength(title, TITLE_LENGTH_LIMIT))
+//            throw new IllegalArgumentException(TITLE_LENGTH_ERROR.getErrorMessage());
+//        // TO.파트장님 !!!!!!!!!!!!!!!!!!!!!!!
+//        // 요기 아래처럼 해도 괜찮나요? Validator 를 사용해서 구현하고 싶었는데 아무래도 list 전체에 접근해야해서 Repository 에서 메소드를 불러왔어요
+//        if (postRepository.isTitleDuplicated(title))
+//            throw new IllegalArgumentException(TITLE_DUPLICATED_ERROR.getErrorMessage());
+//    }
+//
+//    public List<Post> searchPostsByKeyword(final String keyword) {
+//        return postRepository.searchPostsByKeyword(keyword);
+//    }
 }
