146310846fb1313d9b2a485c37ca2ef21b941834
hyeumm <20221159@sungshin.ac.kr>
2025-05-02 19:49:46 +0900

    [Add/#9] add CONTENT_LENGTH_LIMIT

--- src/main/java/org/sopt/constant/LimitConstant.java
+++ src/main/java/org/sopt/constant/LimitConstant.java
@@ -1,6 +1,7 @@
 package org.sopt.constant;
 
 public class LimitConstant {
-    public static final int TITLE_LENGTH_LIMIT = 30;
     public static final int POST_TIME_LIMIT = 3;
+    public static final int TITLE_LENGTH_LIMIT = 30;
+    public static final int CONTENT_LENGTH_LIMIT = 1000;
 }
