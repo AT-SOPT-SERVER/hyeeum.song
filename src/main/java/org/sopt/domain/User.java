530ec188b05449966875620abb652d44d656ce32
hyeumm <20221159@sungshin.ac.kr>
2025-05-02 13:12:42 +0900

    [Feature/#9] User Entity

---
+++ src/main/java/org/sopt/domain/User.java
@@ -0,0 +1,18 @@
+package org.sopt.domain;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> diaryEntities = new ArrayList<>();
+}
