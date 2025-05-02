dfc42dfdbec05a8212c51a09d08d0f27371b7008
hyeumm <20221159@sungshin.ac.kr>
2025-05-02 13:59:11 +0900

    [Feature/#9] UserRepository

--- src/main/java/org/sopt/repository/UserRepository.java
+++ src/main/java/org/sopt/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package org.sopt.repository;
 
+import org.sopt.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User,Long> {
 
 }
