665143e1d114b96dd47f9e6f4e9bd02bf5cb4641
hyeumm <20221159@sungshin.ac.kr>
2025-05-03 12:25:04 +0900

    [Chore/#9] apply UserNameValidator

--- src/main/java/org/sopt/service/UserService.java
+++ src/main/java/org/sopt/service/UserService.java
@@ -1,8 +1,13 @@
 package org.sopt.service;
 
 import org.sopt.domain.User;
+import org.sopt.exception.UserNameBlankException;
+import org.sopt.exception.UserNameLengthException;
 import org.sopt.repository.UserRepository;
+import org.sopt.validator.UserNameValidator;
 import org.springframework.stereotype.Service;
+
+import static org.sopt.constant.LimitConstant.USER_NAME_LENGTH_LIMIT;
 
 @Service
 public class UserService {
@@ -13,8 +18,16 @@ public class UserService {
     }
 
     public void createUser(final String name) {
+        validateUserName(name);
+
         User user = new User(name);
 
         userRepository.save(user);
+    }
+
+    public void validateUserName(final String name) {
+        if (!UserNameValidator.isUserNameBlank(name)) throw new UserNameBlankException();
+        if (!UserNameValidator.isUserNameExceedsLength(name, USER_NAME_LENGTH_LIMIT))
+            throw new UserNameLengthException(USER_NAME_LENGTH_LIMIT);
     }
 }
