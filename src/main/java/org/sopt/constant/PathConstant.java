40b31aaf27b0fca1e8b674064d53c60229cb024a
hyeumm <20221159@sungshin.ac.kr>
2025-06-18 08:28:42 +0900

    [chore/#12] edit PathConstant naming

--- src/main/java/org/sopt/constant/PathConstant.java
+++ src/main/java/org/sopt/constant/PathConstant.java
@@ -3,13 +3,15 @@ package org.sopt.constant;
 public class PathConstant {
     // path
     public static final String POSTS = "/posts";
-    public static final String ID = "/{id}";
+    public static final String POST_ID = "/{post_id}";
     public static final String SEARCH = "/search";
     public static final String USER = "/users";
     public static final String COMMENTS = "/comments";
+    public static final String COMMENT_ID = "/{comment_id}";
 
     // path variable
-    public static final String PATH_ID = "id";
+    public static final String PATH_POST_ID = "post_id";
+    public static final String PATH_COMMENT_ID = "comment_id";
 
     // param
     public static final String PARAM_KEYWORD = "keyword";
