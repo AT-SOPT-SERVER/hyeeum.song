66daee2cfeaa552c4a325dc0e9a126bdf9567bd0
hyeumm <20221159@sungshin.ac.kr>
2025-06-17 19:51:17 +0900

    [chore/#12] change response structure

--- src/main/java/org/sopt/dto/Response/PostResponse.java
+++ src/main/java/org/sopt/dto/Response/PostResponse.java
@@ -2,16 +2,22 @@ package org.sopt.dto.Response;
 
 import org.sopt.domain.Post;
 
+import java.util.List;
+
 public record PostResponse(
-    String title,
-    String content,
-    String author
+        String title,
+        String content,
+        String author,
+        List<CommentResponse> comments
 ) {
     public static PostResponse from(Post post) {
         return new PostResponse(
-            post.getTitle(),
-            post.getContent(),
-            post.getUser().getAuthor()
+                post.getTitle(),
+                post.getContent(),
+                post.getUser().getAuthor(),
+                post.getComments().stream()
+                        .map(CommentResponse::from)
+                        .toList()
         );
     }
 }
